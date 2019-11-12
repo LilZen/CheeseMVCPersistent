c26a8ec108863afcc6cae129b627dad0e07c1512
Melissa <bloomer.melissa18@gmail.com>
2019-11-12 14:45:38 -0500

    Initial commit

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,24 +1,34 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Category;
+import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseType;
+import org.launchcode.cheesemvc.models.data.CategoryDao;
+import org.launchcode.cheesemvc.models.data.CheeseDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value="cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    @Autowired
+    CheeseDao cheeseDao;
+
+    @Autowired
+    CategoryDao categoryDao;
 
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", cheeseDao.findAll());
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
@@ -26,12 +36,68 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+        model.addAttribute("cheeseTypes", CheeseType.values());
         return "cheese/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese, Errors errors, @RequestParam int categoryId, Model model) {
+
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Add Cheese");
+            model.addAttribute("categories", categoryDao.findAll());
+            return "cheese/add";
+        }
+
+        Optional<Category> cat = categoryDao.findById(categoryId);
+        newCheese.setCategory(cat);
+        cheeseDao.save(newCheese);
         return "redirect:";
+    }
+
+    /*    @RequestMapping(value = "edit", method = RequestMethod.GET)
+        public String displayEditForm(Model model, @PathVariable int cheeseId) {
+            model.addAttribute("cheeseId",cheeseDao.findAll());
+            model.addAttribute(new Cheese());
+            model.addAttribute("title", "Edit Cheese NAME (id=ID)");
+        return "cheese/edit";
+        }
+
+        @RequestMapping(value = "edit", method = RequestMethod.POST)
+        public String processEditForm(int cheeseId, String name, String description) {
+            CheeseDao.findAll(cheeseId).setName(name);
+            CheeseDao.findAll(cheeseId).setDescription(description);
+
+             return "redirect:";
+        }
+    */
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model){
+
+        model.addAttribute("cheeses", cheeseDao.findAll());
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam int[] ids) {
+
+        for (int id : ids) {
+      //      cheeseDao.delete(id);
+              cheeseDao.deleteById(id);
+        }
+
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "category", method = RequestMethod.GET)
+    public String category(Model model, @RequestParam int id) {
+
+        Optional<Category> cat = categoryDao.findById(id);
+        List<Cheese> cheeses = cat.getCheeses();
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Cheeses in Category: " + cat.getName());
+        return "cheese/index";
     }
 }
